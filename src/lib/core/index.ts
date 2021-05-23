5622ae1ebf802a1773ce1e984dedae85a12df6fb
Peter Oberrauner <thealmostbearded@gmail.com>
2021-05-23 15:32:13 +0200

    Delete deprecated exports

--- src/lib/core/index.ts
+++ src/lib/core/index.ts
@@ -12,27 +12,6 @@ export * from './series';
 
 export * from './scale';
 
-// export * from './component';
-// export * from './chart-component';
-
-// export * from './mixins/types';
-// export * from './mixins/children-mixin';
-// export * from './mixins/layout-transform-mixin';
-// export * from './mixins/grid-mixin';
-// export * from './mixins/static-size-mixin';
-
-// export * from './components/svg-component';
-// export * from './components/group-component';
-// export * from './components/text-component';
-// export * from './components/rect-component';
-// export * from './components/grid-component';
-// export * from './components/defs-component';
-// export * from './components/clip-path-component';
-// export * from './components/use-component';
-
-// export * from './utility/title-text-attributes';
-// export * from './utility/vertical-text-attributes';
-
 export * from './chart';
 export * from './text';
 
