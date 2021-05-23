0114796a769c15813bcda42d397421c1f809da3b
Peter Oberrauner <thealmostbearded@gmail.com>
2021-05-23 15:21:51 +0200

    Fix bug with const position parameter & add position from x/y attr converter

--- src/lib/core/utility/position.ts
+++ src/lib/core/utility/position.ts
@@ -10,25 +10,28 @@ export function positionToXYAttrs<D>(
   selection: SelectionOrTransition<Element, D>,
   position: Position | ValueFn<Element, D, Position>
 ): void {
-  // todo: comment this function
   const positions: Position[] = new Array(selection.size());
   if (position instanceof Function)
     selection.each((d, i, groups) => (positions[i] = position.call(this, d, i, groups)));
-  else positions.map(() => position);
+  else selection.each((d, i, groups) => (positions[i] = position));
   // note: TS can't handle method chaining when working with SelectionOrTransition
   selection.attr('x', (d, i) => positions[i].x);
   selection.attr('y', (d, i) => positions[i].y);
+}
+
+export function positionFromXYAttrs<D>(selection: SelectionOrTransition<Element, D>): Position {
+  // @ts-ignore TS behaves weirdly with the SelectionOrTransition interface
+  return { x: parseFloat(selection.attr('x') || '0'), y: parseFloat(selection.attr('y') || '0') };
 }
 
 export function positionToTransformAttr<D>(
   selection: SelectionOrTransition<Element, D>,
   position: Position | ValueFn<Element, D, Position>
 ): void {
-  // todo: comment this function
   const positions: Position[] = new Array(selection.size());
   if (position instanceof Function)
     selection.each((d, i, groups) => (positions[i] = position.call(this, d, i, groups)));
-  else positions.map(() => position);
+  else selection.each((d, i, groups) => (positions[i] = position));
 
   selection.attr('transform', (d, i) => `translate(${positions[i].x}, ${positions[i].y})`);
 }
