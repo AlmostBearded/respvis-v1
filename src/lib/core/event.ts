19769e36f572406370728f8d73d9a9c11632090b
Peter Oberrauner <thealmostbearded@gmail.com>
2021-05-28 10:51:40 +0200

    Broadcast events depth first

--- src/lib/core/event.ts
+++ src/lib/core/event.ts
@@ -1,10 +1,11 @@
-import { BaseType, selectAll, Selection } from 'd3-selection';
+import { BaseType, select, selectAll, Selection } from 'd3-selection';
 
 export function eventBroadcast<GElement extends Element, Datum, PElement extends BaseType, PDatum>(
   selection: Selection<GElement, Datum, PElement, PDatum>,
   eventType: string
 ): Selection<GElement, Datum, PElement, PDatum> {
-  return selection.dispatch(eventType).each(function () {
+  return selection.each(function (d, i, g) {
+    select(this).dispatch(eventType);
     eventBroadcast(selectAll(this.children), eventType);
   });
 }
