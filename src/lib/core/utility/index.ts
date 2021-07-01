23ea5d554b7fa39e9b766b4d6646f2039955523e
Peter Oberrauner <thealmostbearded@gmail.com>
2021-07-01 14:06:42 +0200

    Fix wrong condition

--- src/lib/core/utility/index.ts
+++ src/lib/core/utility/index.ts
@@ -2,7 +2,7 @@ export function siblingIndex(node: Element, selector: string = ''): number {
   let index = 0;
   let sibling: Element | null = node;
   while ((sibling = sibling.previousElementSibling)) {
-    if ((selector === '' && sibling.matches(selector)) || selector !== '') index++;
+    if ((selector !== '' && sibling.matches(selector)) || selector === '') index++;
   }
   return index;
 }
