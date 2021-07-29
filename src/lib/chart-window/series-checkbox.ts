8ba7b61d519e926bf70d260cab7c9bbbfd1fdeab
Peter Oberrauner <thealmostbearded@gmail.com>
2021-07-29 17:08:40 +0200

    Add constraints to seriesCheckbox & toolFilterNominal

--- src/lib/chart-window/series-checkbox.ts
+++ src/lib/chart-window/series-checkbox.ts
@@ -1,15 +1,19 @@
 import { EnterElement, select, Selection, ValueFn } from 'd3-selection';
-import { DataSeriesGenerator, uuid } from '../core';
+import { DataSeriesGenerator, siblingIndexSameClasses, uuid } from '../core';
 
 export interface DataCheckbox {
   label: string;
   checked: boolean;
+  disabled: boolean;
 }
 
 export interface DataSeriesCheckbox extends DataSeriesGenerator<DataCheckbox> {
   container: string | ValueFn<EnterElement, DataCheckbox, HTMLElement>;
   labels: string[];
   checked: boolean[];
+  disabled?: boolean[];
+  minChecked: number;
+  maxChecked: number;
 }
 
 export function dataSeriesCheckbox(data: Partial<DataSeriesCheckbox>): DataSeriesCheckbox {
@@ -17,6 +21,9 @@ export function dataSeriesCheckbox(data: Partial<DataSeriesCheckbox>): DataSerie
     container: data.container || 'div',
     labels: data.labels || [],
     checked: data.checked || [],
+    disabled: data.disabled,
+    minChecked: data.minChecked || 0,
+    maxChecked: data.maxChecked || Infinity,
     dataGenerator: data.dataGenerator || dataSeriesCheckboxGenerator,
   };
 }
@@ -24,15 +31,30 @@ export function dataSeriesCheckbox(data: Partial<DataSeriesCheckbox>): DataSerie
 export function dataSeriesCheckboxGenerator(
   selection: Selection<Element, DataSeriesCheckbox>
 ): DataCheckbox[] {
-  const seriesDatum = selection.datum();
-  return seriesDatum.labels.map((l, i) => ({ label: l, checked: seriesDatum.checked[i] }));
+  const { labels, checked, disabled, minChecked, maxChecked } = selection.datum();
+  const checkedCount = checked.reduce((count, checked) => count + (checked ? 1 : 0), 0);
+  return labels.map((l, i) => {
+    return {
+      label: l,
+      checked: checked[i],
+      disabled:
+        disabled?.[i] ||
+        (checked[i] && checkedCount <= minChecked) ||
+        (!checked[i] && checkedCount >= maxChecked),
+    };
+  });
 }
 
 export function seriesCheckbox(selection: Selection<HTMLElement, DataSeriesCheckbox>): void {
   selection
     .classed('series-checkbox', true)
     .on('change.seriescheckbox', function (e, d) {
-      d.checked = select(this).selectAll('input[type=checkbox]').properties('checked');
+      const checkbox = <HTMLInputElement>e.target;
+      const index = siblingIndexSameClasses(checkbox.closest('.checkbox')!);
+      select<Element, DataSeriesCheckbox>(this).datum((d) => {
+        d.checked[index] = checkbox.checked;
+        return d;
+      });
     })
     // toggle checkbox by clicking on on containers
     .on(
@@ -57,15 +79,26 @@ export function seriesCheckbox(selection: Selection<HTMLElement, DataSeriesCheck
               })
               .call((s) => series.dispatch('enter', { detail: { selection: s } })),
           undefined,
-          (exit) =>
-            exit.remove().call((s) => series.dispatch('exit', { detail: { selection: s } }))
+          (exit) => exit.remove().call((s) => series.dispatch('exit', { detail: { selection: s } }))
         )
         .each((d, i, g) => {
           const s = select(g[i]);
-          s.selectChildren('input').property('checked', d.checked);
+          s.selectChildren('input')
+            .attr('checked', <any>d.checked || null)
+            .attr('disabled', <any>d.disabled || null);
           s.selectChildren('label').text(d.label);
         })
         .call((s) => series.dispatch('update', { detail: { selection: s } }));
     })
     .dispatch('datachange');
 }
+
+// function seriesCheckboxEnsureConstraints(series: Selection<Element, DataSeriesCheckbox>) : void {
+//   const checkboxS = select(this).selectAll('input[type=checkbox]'),
+//   checkedCheckboxS = checkboxS.filter('[checked=true]'),
+//   uncheckedCheckboxS = checkboxS.filter(':not[checked=true]');
+// series.checked = checkboxS.properties('checked');
+// const checkedCount = d.checked.reduce((count, checked) => count + (checked ? 1 : 0), 0);
+// if (checkedCount === d.minChecked) checkedCheckboxS.;
+
+// }
