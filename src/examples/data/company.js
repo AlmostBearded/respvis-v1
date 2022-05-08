2b252cd0f641c8a5561f50f26cc0437a4281249f
Peter Oberrauner <thealmostbearded@gmail.com>
2022-05-08 16:09:30 +0200

    Improve responsive grouped bar chart example

--- src/examples/data/company.js
+++ src/examples/data/company.js
@@ -1,11 +1,15 @@
+export const sites = ['Vienna', 'Rome', 'Paris', 'Berlin', 'Amsterdam'];
+export const years = ['2018', '2019', '2020'];
+export const revenues = [
+  [23000000, 27000000, 29000000],
+  [15000000, 13000000, 16000000],
+  [43000000, 55000000, 70000000],
+  [30000000, 24000000, 28000000],
+  [12000000, 14000000, 18000000],
+];
+
 export default {
-  sites: ['Vienna', 'Rome', 'Paris', 'Berlin', 'Amsterdam'],
-  years: ['2018', '2019', '2020'],
-  revenues: [
-    [23, 27, 29],
-    [15, 13, 16],
-    [43, 55, 70],
-    [30, 24, 28],
-    [12, 14, 18],
-  ],
+  sites,
+  years,
+  revenues,
 };
